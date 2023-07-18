f4329590310e7128ec4ad51abcb18b3c76388ffa
flutcla <flutcla@gmail.com>
2023-07-18 14:37:50 +0900

    todo-list コンポーネントの作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './shared/components/toolbar/toolbar/toolbar.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TodoListComponent,
   ],
   imports: [
     BrowserModule,
