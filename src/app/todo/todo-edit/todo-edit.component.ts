3b58bd23beff5244060f70d9d6638eeb7eba2bdc
flutcla <flutcla@gmail.com>
2023-07-25 13:51:16 +0900

    /todo/edit/:id を実装

--- src/app/todo/todo-edit/todo-edit.component.ts
+++ src/app/todo/todo-edit/todo-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Category, DataService, TodoCategory, STATUS, Todo, TodoEdit } from 'src/app/data.service';
 
 @Component({
   selector: 'app-todo-edit',
@@ -6,5 +10,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-edit.component.scss']
 })
 export class TodoEditComponent {
+  id?: number;
+  subs: Subscription[] = [];
+  todoCategory?: TodoCategory;
+  categoryList: Category[] = [];
+  errorMessage?: String;
+  status = STATUS;
 
+  constructor(
+    private route: ActivatedRoute,
+    private dataService: DataService,
+    private builder: FormBuilder,
+    private router: Router,
+  ) {}
+
+  form = this.builder.group({
+    categoryId: ['', Validators.required],
+    title: ['', Validators.required],
+    body: [''],
+    state: ['', Validators.required]
+  })
+
+  ngOnInit(): void {
+    this.subs.push(
+      this.route.params.subscribe(params => {
+        this.id = Number(params['id']);
+      })
+    );
+
+    this.subs.push(
+      this.dataService.getCategoryList().subscribe(data => {
+        this.categoryList = data;
+      })
+    );
+
+    this.subs.push(
+      this.dataService.getTodoCategoryList().subscribe(data => {
+        this.todoCategory = data.find(elem => elem.todo.id == this.id);
+        console.log(this.todoCategory)
+        if(this.todoCategory != undefined){
+          this.form.setValue({
+            categoryId: String(this.todoCategory.todo.categoryId),
+            title: String(this.todoCategory.todo.title),
+            body: String(this.todoCategory.todo.body),
+            state: String(this.todoCategory.todo.state.code)
+          });
+        }
+      })
+    );
+  }
+
+  onSubmit(): void {
+    const formData = this.form.value;
+    console.log(formData)
+    if (this.form.valid){
+      const todoData: TodoEdit = {
+        id: this.id!,
+        categoryId: Number(formData.categoryId!),
+        title: formData.title!,
+        body: formData.body ?? "",
+        state: Number(formData.state),
+      }
+      this.subs.push(
+        this.dataService.updateTodo(todoData).subscribe({
+          next: (_) => {
+            this.router.navigate(['todo/list']);
+          },
+          error: (e) => {
+            this.errorMessage = e.message;
+          }
+        })
+      );
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach(s => s.unsubscribe());
+  }
 }
