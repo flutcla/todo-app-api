0037425d5686c2b943edc7058f952016d16b654e
flutcla <flutcla@gmail.com>
2023-07-28 11:12:26 +0900

    RouterModule を読み込むよう修正

--- src/app/shared/components/toolbar/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -11,7 +12,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   imports: [
     MatIconModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule
   ]
 })
 export class ToolbarComponent {
