52ef245c61e85716ea4831c74c7f960388cfea07
Kosuke Kiuchi <69044947+flutcla@users.noreply.github.com>
2023-08-08 16:51:08 +0900

    todo-list で ngxs を利用

---
+++ src/app/shared/store/todo.state.ts
@@ -0,0 +1,90 @@
+import { Injectable } from "@angular/core";
+import { State, Action, StateContext, Selector } from '@ngxs/store';
+import { TodoCategory } from "src/app/data.service";
+import { TodoAction } from "./todo.action";
+import { DataService } from "src/app/data.service";
+import { tap, finalize } from "rxjs";
+
+export class TodoCategoryStateModel {
+  todoCategories?: TodoCategory[];
+  selectedTodo?: TodoCategory;
+  isLoading?: boolean;
+}
+
+@State<TodoCategoryStateModel>({
+  name: 'todoCategories',
+  defaults: {
+    todoCategories: [],
+    selectedTodo: undefined,
+    isLoading: false,
+  },
+})
+
+@Injectable()
+export class TodoCategoryState {
+  constructor(private dataService: DataService) {}
+
+  @Action(TodoAction.GetAll)
+  getTodoAll(ctx: StateContext<TodoCategoryStateModel>) {
+    ctx.patchState({ isLoading: true });
+    return this.dataService.getTodoCategoryList().pipe(
+      tap(data => {
+        ctx.patchState({ todoCategories: data });
+      }),
+      finalize(() => {
+        ctx.patchState({ isLoading: false });
+      })
+    );
+  }
+
+  @Action(TodoAction.Get)
+  getTodo(ctx: StateContext<TodoCategoryStateModel>, action: TodoAction.Get) {
+    return this.dataService.getTodoCategoryList().pipe(
+      tap(data => {
+        ctx.patchState({ selectedTodo: data.find(x => x.todo.id == action.id) });
+      })
+    );
+  }
+
+  @Action(TodoAction.Add)
+  addTodo(ctx: StateContext<TodoCategoryStateModel>, action: TodoAction.Add) {
+    return this.dataService.storeTodo(action.todoStore).pipe(
+      tap(data => {
+        ctx.dispatch(new TodoAction.GetAll());
+      })
+    );
+  }
+
+  @Action(TodoAction.Delete)
+  deleteTodo(ctx: StateContext<TodoCategoryStateModel>, action: TodoAction.Delete) {
+    return this.dataService.deleteTodo(action.todo).pipe(
+      finalize(() => {
+        ctx.dispatch(new TodoAction.GetAll())
+      })
+    );
+  }
+
+  @Action(TodoAction.Update)
+  updateTodo(ctx: StateContext<TodoCategoryStateModel>, action: TodoAction.Update) {
+    return this.dataService.updateTodo(action.todoUpdate).pipe(
+      finalize(() => {
+        ctx.dispatch(new TodoAction.GetAll())
+      })
+    );
+  }
+
+  @Selector()
+  static todoCategories(state: TodoCategoryStateModel) {
+    return state.todoCategories;
+  }
+
+  @Selector()
+  static selectedTodo(state: TodoCategoryStateModel) {
+    return state.selectedTodo;
+  }
+
+  @Selector()
+  static isLoading(state: TodoCategoryStateModel) {
+    return state.isLoading;
+  }
+}
--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,36 +1,31 @@
-import { Component } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { DataService } from 'src/app/data.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store, Select } from '@ngxs/store';
 import { Todo, TodoCategory } from 'src/app/data.service';
+import { TodoCategoryState } from 'src/app/shared/store/todo.state';
+import { TodoAction } from 'src/app/shared/store/todo.action';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
 })
 
-export class TodoListComponent {
-  constructor(private dataService: DataService) {
-    this.isLoading = true;
-  }
+export class TodoListComponent implements OnInit {
+  @Select(TodoCategoryState.todoCategories) todoCategories$?: Observable<TodoCategory[]>
+  @Select(TodoCategoryState.isLoading) isLoading$?: Observable<boolean>
 
-  isLoading: boolean;
-  todoCategoryList: TodoCategory[] = [];
-  subscription = new Subscription();
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.subscription.add(this.dataService.getTodoCategoryList().subscribe(data => {
-      this.todoCategoryList = data;
-      this.isLoading = false;
-    }));
+    this.getTodoCategories();
+  }
+
+  getTodoCategories(): void {
+    this.store.dispatch(new TodoAction.GetAll());
   }
 
   delete(todo: Todo) {
-    this.subscription.add(this.dataService.deleteTodo(todo).subscribe());
-    window.location.reload();
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.store.dispatch(new TodoAction.Delete(todo))
   }
 }
 
