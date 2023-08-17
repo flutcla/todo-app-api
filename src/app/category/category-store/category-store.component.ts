2fb10dcf97d7e40b0ba608e2c044bbdda05d4128
Kosuke Kiuchi <69044947+flutcla@users.noreply.github.com>
2023-08-17 16:52:09 +0900

    category-store で ngxs を利用

--- src/app/category/category-store/category-store.component.ts
+++ src/app/category/category-store/category-store.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { CategoryStore, DataService } from 'src/app/data.service';
+import { CategoryStore } from 'src/app/data.service';
 import { Color } from '@angular-material-components/color-picker';
+import { Store } from '@ngxs/store';
+import { CategoryAction } from 'src/app/shared/store/category.action';
 
 @Component({
   selector: 'app-category-store',
@@ -11,17 +13,17 @@ import { Color } from '@angular-material-components/color-picker';
   styleUrls: ['./category-store.component.scss'],
 })
 export class CategoryStoreComponent {
+  subs = new Subscription();
+
   constructor(
-    private dataService: DataService,
+    private store: Store,
     private builder: FormBuilder,
     private router: Router
   ) {}
 
-  subscription = new Subscription();
-
   form = this.builder.group({
-    name: ["", Validators.required],
-    slug: ["", Validators.required],
+    name: new FormControl<string|null>(null, [Validators.required]),
+    slug: new FormControl<string|null>(null, [Validators.required]),
     color: new FormControl(new Color(255, 255, 255), [Validators.required])
   });
 
@@ -38,9 +40,9 @@ export class CategoryStoreComponent {
         slug: formData.slug!,
         color: formData.color!.hex
       }
-      this.subscription.add(this.dataService.storeCategory(categoryStoreData).subscribe({
+      this.subs.add(this.store.dispatch(new CategoryAction.Add(categoryStoreData)).subscribe({
         next: (_) => {
-          this.router.navigate(['todo/list']);
+          this.router.navigate(['category/list']);
         },
         error: (e) => {
           this.errorMessage = e.message;
@@ -50,6 +52,6 @@ export class CategoryStoreComponent {
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subs.unsubscribe();
   }
 }
